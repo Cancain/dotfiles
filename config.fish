5adc40cf018240ed2d6ec434d39b0275311484e6
Tomas Eriksson <eriksson.tomas@gmail.com>
2021-02-23 11:24:10 +0100

    i3config

---
+++ config.fish
@@ -0,0 +1,2 @@
+set -gx PATH ~/bin $PATH
+set -gx ANDROID_SDK_ROOT /opt/android-sdk
--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -11,10 +11,17 @@ mkdir -p ~/.config/nvim
 [ ! -f ~/.config/nvim/init.vim ] && ln -s $DIR/init.vim ~/.config/nvim/init.vim
 [ ! -f ~/.vimrc ] && ln -s $DIR/init.vim ~/.vimrc
 
+# terminator
+mkdir -p ~/.config/terminator
+[ ! -f ~/.config/terminator/config ] && ln -s $DIR/terminator ~/.config/terminator/config
+
 # rg
 [ ! -f ~/.rgrc ] && ln -s $DIR/rgrc ~/.rgrc
 
 # xinit
 [ ! -f ~/.xinitrc ] && ln -s $DIR/xinitrc ~/.xinitrc
+
+mkdir -p ~/.config/fish
+[ ! -f ~/.config/fish/config.fish ] && ln -s $DIR/config.fish ~/.config/fish/config.fish
 
 exit 0
