1fca965bd13bd707e3ba10985602f741ca23b765
Tomas Eriksson <eriksson.tomas@gmail.com>
2021-04-14 16:26:45 +0200

    added more aliases

--- config.fish
+++ config.fish
@@ -9,13 +9,17 @@ alias yh="yarn_here"
 alias connect="cd /home/tomeri/connect/"
 alias native="cd /home/tomeri/connect/native"
 alias web="cd /home/tomeri/connect/web"
+alias shared="cd /home/tomeri/connect/shared"
 alias scr="./scripts/screens.sh"
 alias kdiscord="pkill -9 -f discord"
 alias webs="cd /home/tomeri/connect/web && yarn start"
 alias natives="cd /home/tomeri/connect/native && yarn start"
 alias ra="cd /home/tomeri/connect/native && yarn ra"
 alias codecon="code /home/tomeri/connect"
+alias coden="code /home/tomeri/connect/native"
+alias codew="code /home/tomeri/connect/web"
 alias todo="/home/tomeri/todoist/todoist"
+alias rmerged="/home/tomeri/Scripts/rmerged"
 
 export EDITOR=nvim
 
